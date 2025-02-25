65a7d9f6aff3d1c03a7e4b62c21a587f9286a41d
sshreya <shreyag.1203@gmail.com>
2025-02-25 10:37:06 +0200

    Create schema.sql

---
+++ Cohort Retention Analysis/schema.sql
@@ -0,0 +1,10 @@
+create table online_retail(
+InvoiceNo varchar(10),
+StockCode varchar(20),
+Description varchar(50),
+Quantity int,
+InvoiceDate varchar(20),
+UnitPrice varchar(10),
+CustomerID varchar(10),
+Country varchar(50)
+);
